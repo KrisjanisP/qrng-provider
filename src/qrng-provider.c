365dfa3c5b4f571ee852617bbfd76838a7be979f
KrisjanisP <krisjanispetrucena@gmail.com>
2022-11-24 13:19:46 +0200

    format

--- src/qrng-provider.c
+++ src/qrng-provider.c
@@ -6,19 +6,19 @@
 
 #define QRNG_PROV_NAME "RQRNG provider"
 
-struct QRNG_PROVIDER_CTX {
+struct QRNG_PROVIDER_CTX
+{
     const OSSL_CORE_HANDLE *core;
     OSSL_LIB_CTX *libctx;
 };
-
-
 
 static void qrng_unquery_operation(void *provctx, int operation_id, const OSSL_ALGORITHM *alg)
 {
     OPENSSL_free((void *)alg);
 }
 
-static const OSSL_ALGORITHM* qrng_query_operation(void* provctx, int operation_id, const int* no_store){
+static const OSSL_ALGORITHM *qrng_query_operation(void *provctx, int operation_id, const int *no_store)
+{
     return NULL;
 }
 
@@ -34,18 +34,16 @@ static int qrng_get_params(void *provctx, OSSL_PARAM params[])
 }
 
 static const OSSL_DISPATCH qrng_dispatch_table[] = {
-    { OSSL_FUNC_PROVIDER_QUERY_OPERATION, (void (*)(void))qrng_query_operation },
-    { OSSL_FUNC_PROVIDER_UNQUERY_OPERATION, (void (*)(void))qrng_unquery_operation},
-    { OSSL_FUNC_PROVIDER_GET_PARAMS, (void (*)(void))qrng_get_params },
-    { 0, NULL }
-};
+    {OSSL_FUNC_PROVIDER_QUERY_OPERATION, (void (*)(void))qrng_query_operation},
+    {OSSL_FUNC_PROVIDER_UNQUERY_OPERATION, (void (*)(void))qrng_unquery_operation},
+    {OSSL_FUNC_PROVIDER_GET_PARAMS, (void (*)(void))qrng_get_params},
+    {0, NULL}};
 
-
-int OSSL_provider_init(const OSSL_CORE_HANDLE* handle,
-                       const OSSL_DISPATCH* in, const OSSL_DISPATCH** out,
-                       void** provctx)
+int OSSL_provider_init(const OSSL_CORE_HANDLE *handle,
+                       const OSSL_DISPATCH *in, const OSSL_DISPATCH **out,
+                       void **provctx)
 {
-    struct QRNG_PROVIDER_CTX* cprov;
+    struct QRNG_PROVIDER_CTX *cprov;
 
     if ((cprov = OPENSSL_zalloc(sizeof(struct QRNG_PROVIDER_CTX))) == NULL)
         return 0;
